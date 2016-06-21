5597b55c8fa827d68b0da419c6aff8306861677d
20141105053w <20141105053w@admindeiMac-88.local>
2016-06-21 09:47:11 +0800

    group
    定义一个由20个整形元素组成的数组，利用冒泡排序进行排序。首先在网上查找如何在swift中定义数组，之后进行冒泡排序，并输出排序后的数组。

---
+++ group/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  group
+//
+//  Created by 20141105053w on 16/6/21.
+//  Copyright © 2016年 20141105053w. All rights reserved.
+//
+
+import Foundation
+
+
+var arr=[Int](arrayLiteral:34,554,2,23,45,32,45,6,786,5,43,55,78,96,456,657,56,88,99,51)//根据在网上的学习，自己定义的数组
+
+//print(arr,count)
+
+
+//var array = [123,234,12,33,67,346,4,75,67,234,23,1233,3,5,986,98,567,345,234,234]  /／在网上找的资料，经过试验，两种定义均能使用
+let count = arr.count//统计数组中的元素个数
+print("排序前的值,(共：",count,"个)")
+
+for item in arr
+{
+    var ii = item
+    print(ii)
+}
+
+for var i = 0; i < arr.count - 1; ++i
+{
+    for var j = 0; j < arr.count - 1 - i; ++j
+    {
+        if arr[j] > arr[j + 1]
+        {
+            var temp = arr[j + 1]
+            arr[j + 1] = arr[j]
+            arr[j] = temp
+        }
+    }
+}
+
+print("排序后的值：")
+
+for item in arr
+{
+    var ii = item
+    print(ii)
+}
